9dc9eef829ee4bd84c525a0b71d0c39f713d0b31
Babashov Mahammad <babashov.info@gmail.com>
2023-01-06 13:01:57 +0400

    Memory leak (this issue is solved in react 18)

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,30 @@
-
+import { useState, useEffect, useRef } from "react"
 
 function Todo() {
-  return (
-    <div>Todo</div>
-  )
+    const [todo,setTodo] = useState({});
+    const [loading,setLoading] = useState(true);
+    const isMounted = useRef(true);
+
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(res=>res.json())
+        .then(data=>{
+            setTimeout(()=>{
+                if(isMounted){
+                    // Memory Leak issue is solved in react 18
+                }
+                setTodo(data)
+                setLoading(false);
+                
+            },3000)
+        })
+        .catch(err=>console.log(err.message))
+
+        return ()=>{
+            isMounted.current = false;
+        }
+    })
+  return loading ? <h3>Loading</h3> : <p>{todo.title}</p>
 }
 
 export default Todo
--- src/components/UseRefExample3.jsx
+++ src/components/UseRefExample3.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react"
 import Todo from "./Todo"
 
 function UseRefExample3() {
+
+    const [showTodo,setShowTodo] = useState(true);
+
   return (
     <div>
-        <Todo/>
+        {showTodo && <Todo />}
+        
+        <button onClick={e=>setShowTodo(!showTodo)} className="btn btn-primary mt-3">
+            {showTodo ? "Hide" : "Show"}
+        </button>
     </div>
   )
 }
